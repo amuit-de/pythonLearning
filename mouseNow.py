0f491793d697e858c4fa56f627827d91e0ef77c2
Alexander Mueller <alexander.mueller@viacor.de>
2019-10-02 08:28:23 +0200

    new end

--- mouseNow.py
+++ mouseNow.py
@@ -2,6 +2,8 @@
 # mouseNow.py  -  display the current position of the mouse pointer
 
 import pyautogui
+import time
+
 print('Press Ctrl-C to quit.')
 
 try:
@@ -10,6 +12,7 @@ try:
         x, y = pyautogui.position()   # Position Mauszeiger abrufen
         positionStr = 'X:  ' + str(x).rjust(4) +  '   Y: ' + str(y).rjust(4)
         print(positionStr, end='')
+        time.sleep(0.50)
         print('\b' * len(positionStr), end='', flush=True)
 except KeyboardInterrupt:
     print('\nDone.') 
