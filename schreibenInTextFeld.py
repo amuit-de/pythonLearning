da37b88db4b075f2b706b060e229148220a9901f
Alexander Mueller <alexander.mueller@viacor.de>
2019-10-11 14:03:05 +0200

    test pyperclip

--- schreibenInTextFeld.py
+++ schreibenInTextFeld.py
@@ -1,3 +1,13 @@
 import pyautogui
-pyautogui.click(153,332); pyautogui.typewrite('Hello World', 0.25)
-pyautogui.typewrite(['a', 'b', 'left', 'left','X', 'Y'] , 0.25)
+import pyperclip
+
+
+pyautogui.click(153,332); 
+#pyautogui.typewrite('Hello World', 0.25)
+# pyautogui.typewrite(['a', 'b', 'left', 'left','X', 'Y'] , 0.25)
+
+testString = ('1234.ü.ö.ß.Ä.Ö.Ü.912345.789')
+testString2 = ('123456789012345678901234567890')
+pyperclip.copy(str(testString) [0:25])
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.typewrite(testString)
